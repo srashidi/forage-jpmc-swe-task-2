75cdfe24d7b47a6e910806db91b9087a0cb2a9a9
Shaunty Rashidi <srashidi@users.noreply.github.com>
2024-05-16 13:44:42 -0700

    Edit Graph.tsx to make view into y_line instead of y-line

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -49,7 +49,7 @@ class Graph extends Component<IProps, {}> {
 
       // Add more Perspective configurations here.
       elem.load(this.table);
-      elem.setAttribute('view', 'y-line');
+      elem.setAttribute('view', 'y_line');
       elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
       elem.setAttribute('columns', '["top_ask_price"]');
